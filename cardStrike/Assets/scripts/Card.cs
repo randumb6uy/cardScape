a580f8c19ff275520dd0807f4a01e595c754a39f
Trollface <tusharpaul883@gmail.com>
2021-06-15 16:02:44 +0530

    initial commit
    contains assets and project settings

---
+++ cardStrike/Assets/scripts/Card.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "card Template", menuName = "card")]
+public class Card : ScriptableObject 
+{
+ public string _name;
+ public string description;
+ public int cost;
+ public Sprite image;   
+}
