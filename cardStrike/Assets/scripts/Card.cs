8284a08124cccfadde514a5238d56d502d1a9ea7
randumb6uy <tush1066@gmail.com>
2021-06-16 17:23:15 +0530

    extras
    extras addintion cards

--- cardStrike/Assets/scripts/Card.cs
+++ cardStrike/Assets/scripts/Card.cs
@@ -5,8 +5,12 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "card Template", menuName = "card")]
 public class Card : ScriptableObject 
 {
- public string _name;
- public string description;
- public int cost;
- public Sprite image;   
+    public string _name;
+    public string description;
+    public int cost;
+    public Sprite image;   
+    public void action()
+    {
+                
+    }
 }
